7f5bc5505d36c87fb3ce09755a4ce7422a1e4499
jackcobb <jack3@vt.edu>
2019-04-20 08:52:43 -0400

    Fixing set bug

--- Keystore.java
+++ Keystore.java
@@ -32,7 +32,7 @@ public class Keystore {
 					break;
 				case "set":
 					String key = nextLineArgs[1];
-					String value = nextLineArgs[0];
+					String value = nextLineArgs[2];
 					System.out.println(String.format("Adding key-value pair '%s'-'%s' to keystore", key, value));
 					keystore.put(key, value);
 					break;
